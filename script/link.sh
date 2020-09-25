62d879f5c2850fab5aea488c69b8fee363fea43d
shunk1mura <shun.kimura@ghelia.com>
2020-09-25 08:30:33 +0000

    とりあえず

--- script/link.sh
+++ script/link.sh
@@ -3,6 +3,7 @@
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 
     ln -s $HOME/dotfiles/$f $HOME/$f
