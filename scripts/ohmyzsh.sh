d47ac331daddc08fd2710b2633e60636a2e2900b
tj16kimura <kawalab.19.kimura@gmail.com>
2021-10-28 13:22:46 +0900

    fix arg

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc --runzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc --unattended
 
 # syntax highlighting plugin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
